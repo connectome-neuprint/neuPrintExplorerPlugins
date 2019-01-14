b42f92e06bcb2b6e2084cf4c37f150cfd41bd67c
neubarthn <nicoleneubarth@gmail.com>
2019-01-14 13:59:46 -0500

    created global actions for testing queries; setupJest globally imports common libraries

--- packages/query/setupJest.js
+++ packages/query/setupJest.js
@@ -1,5 +1,35 @@
-import { configure } from 'enzyme';
+/* eslint-disable import/no-extraneous-dependencies */
 import Adapter from 'enzyme-adapter-react-16';
+import merge from 'deepmerge';
+import React from 'react';
+import enzyme from 'enzyme';
+import renderer from 'react-test-renderer';
+
 global.fetch = require('jest-fetch-mock');
 
-configure({ adapter: new Adapter() });
+global.React = React;
+global.enzyme = enzyme;
+global.renderer = renderer;
+
+enzyme.configure({ adapter: new Adapter() });
+
+global.queryStringObject = {};
+const overwriteMerge = (destinationArray, sourceArray) => sourceArray;
+
+// these should match actions available in QueryForm.jsx in neuPrintExplorer
+global.actions = {
+  submit: jest.fn(),
+  skeletonAddandOpen: jest.fn(),
+  neuroglancerAddandOpen: jest.fn(),
+  formError: jest.fn(),
+  metaInfoError: jest.fn(),
+  pluginResponseError: jest.fn(),
+  getQueryString: jest.fn(),
+  getSiteParams: jest.fn(),
+  setQueryString: jest.fn(newData => {
+    global.queryStringObject = merge(global.queryStringObject, newData, {
+      arrayMerge: overwriteMerge
+    });
+  }),
+  getQueryObject: jest.fn(() => global.queryStringObject)
+};
