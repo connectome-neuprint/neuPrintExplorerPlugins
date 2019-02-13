1525f3c1b36e66447d1bdfc0c57bc1c97dede991
neubarthn <nicoleneubarth@gmail.com>
2019-02-13 11:22:57 -0500

    created independent table component that handles its own state rather than relying on visProps. used for views containing a table as a subcomponent

---
+++ packages/view/src/visualization/IndependentTable.jsx
@@ -0,0 +1,243 @@
+// Table that handles its own state
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import { withStyles } from '@material-ui/core/styles';
+
+import TablePaginationActions from '@neuprint/support';
+
+function desc(a, b, orderBy) {
+  let aVal = a[orderBy];
+  let bVal = b[orderBy];
+
+  // need to check if the cell has a value / action object
+  if (aVal && typeof aVal === 'object') {
+    if ('sortBy' in aVal) {
+      aVal = aVal.sortBy;
+    } else if ('value' in aVal) {
+      aVal = aVal.value;
+    }
+  }
+
+  if (bVal && typeof bVal === 'object') {
+    if ('sortBy' in bVal) {
+      bVal = bVal.sortBy;
+    } else if ('value' in bVal) {
+      bVal = bVal.value;
+    }
+  }
+
+  // need to check for null values
+  if (bVal === null) {
+    return 1;
+  }
+  if (aVal === null) {
+    return -1;
+  }
+
+  // now finish the sort
+  if (bVal < aVal) {
+    return -1;
+  }
+  if (bVal > aVal) {
+    return 1;
+  }
+  return 0;
+}
+
+function stableSort(array, cmp) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = cmp(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+
+function getSorting(order, orderBy) {
+  return order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
+}
+
+const styles = theme => ({
+  root: {
+    width: '100%'
+  },
+  clickable: {
+    cursor: 'pointer'
+  },
+  scroll: {
+    marginTop: theme.spacing.unit * 1,
+    overflowY: 'auto',
+    overflowX: 'auto'
+  }
+});
+
+class IndependentTable extends React.Component {
+  constructor(props) {
+    super(props);
+    const { paginate, page, orderBy, order, rowsPerPage } = this.props;
+    this.state = {
+      paginate,
+      page,
+      orderBy,
+      order,
+      rowsPerPage
+    };
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    const { data } = this.props;
+    if (nextProps.data === data && nextState === this.state) {
+      return false;
+    }
+    return true;
+  }
+
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
+
+  handleChangeRowsPerPage = event => {
+    this.setState({ rowsPerPage: event.target.value });
+  };
+
+  handleCellClick = action => () => {
+    action();
+  };
+
+  handleRequestSort = property => () => {
+    const { orderBy, order } = this.state;
+
+    const newOrderBy = property;
+    const newOrder = orderBy === property && order === 'desc' ? 'asc' : 'desc';
+
+    this.setState({ order: newOrder, orderBy: newOrderBy });
+  };
+
+  render() {
+    const { data, columns, disableSort, classes, highlightIndex } = this.props;
+    let { rowsPerPage } = this.state;
+    const { paginate, orderBy, order, page } = this.state;
+
+    // fit table to data
+    if (data.length < rowsPerPage || paginate === false) {
+      rowsPerPage = data.length;
+    }
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
+
+    return (
+      <div className={classes.root}>
+        <div className={classes.scroll}>
+          <Table padding="dense">
+            <TableHead>
+              <TableRow>
+                {columns.map((header, index) => {
+                  const headerKey = `${header}${index}`;
+                  if (disableSort.size > 0 && disableSort.has(index)) {
+                    return <TableCell key={headerKey}>{header}</TableCell>;
+                  }
+                  return (
+                    <TableCell key={headerKey} sortDirection={orderBy === index ? order : false}>
+                      <TableSortLabel
+                        active={orderBy === index}
+                        direction={order}
+                        onClick={this.handleRequestSort(index)}
+                      >
+                        {header}
+                      </TableSortLabel>
+                    </TableCell>
+                  );
+                })}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {stableSort(data, getSorting(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  let rowStyle = {};
+                  const currspot = page * rowsPerPage + index;
+                  if (highlightIndex && currspot.toString() in highlightIndex) {
+                    rowStyle = { backgroundColor: highlightIndex[currspot.toString()] };
+                  }
+                  const key = index;
+                  return (
+                    <TableRow hover key={key} style={rowStyle}>
+                      {row.map((cell, i) => {
+                        if (cell && typeof cell === 'object' && 'value' in cell) {
+                          const cellKey = `${i}${cell.value}`;
+                          if ('action' in cell) {
+                            return (
+                              <TableCell
+                                className={classes.clickable}
+                                key={cellKey}
+                                onClick={this.handleCellClick(cell.action)}
+                              >
+                                {cell.value}
+                              </TableCell>
+                            );
+                          }
+                          return <TableCell key={cellKey}>{cell.value}</TableCell>;
+                        }
+                        const cellKey = `${i}${cell}`;
+                        return <TableCell key={cellKey}>{cell}</TableCell>;
+                      })}
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 48 * emptyRows }}>
+                  <TableCell key="empty" colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+        {paginate ? (
+          <TablePagination
+            component="div"
+            count={data.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onChangePage={this.handleChangePage}
+            onChangeRowsPerPage={this.handleChangeRowsPerPage}
+            rowsPerPageOptions={[5, 10, 25, 50, 100]}
+            ActionsComponent={TablePaginationActions}
+          />
+        ) : null}
+      </div>
+    );
+  }
+}
+
+IndependentTable.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.array).isRequired,
+  columns: PropTypes.arrayOf(PropTypes.string).isRequired,
+  paginate: PropTypes.bool,
+  page: PropTypes.number,
+  orderBy: PropTypes.string,
+  order: PropTypes.string,
+  rowsPerPage: PropTypes.number,
+  classes: PropTypes.object.isRequired,
+  disableSort: PropTypes.object,
+  highlightIndex: PropTypes.object
+};
+
+IndependentTable.defaultProps = {
+  paginate: true,
+  page: 0,
+  orderBy: '',
+  order: 'asc',
+  rowsPerPage: 5,
+  disableSort: new Set([]),
+  highlightIndex: {}
+};
+
+export default withStyles(styles)(IndependentTable);
