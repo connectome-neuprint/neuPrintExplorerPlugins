a4522b92ef8eab047a533f9afb6848a10a64de2a
Jody Clements <jody@neomorphic.co.uk>
2019-09-13 09:40:50 -0400

    Changes color of 'eye' icon.
    The 'eye' icon color has been changed to match the link color used in
    the SimpleTable view. This helps to show that it too is a link.

--- packages/query/src/visualization/BodyId.jsx
+++ packages/query/src/visualization/BodyId.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Icon from '@material-ui/core/Icon';
 import SelectAndCopyText from '../shared/SelectAndCopyText';
+
+const styles = theme => ({
+  icon: {
+    marginLeft: '3px',
+    cursor: 'pointer',
+    color: theme.palette.primary.main
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'row'
+  }
+});
 
 /**
  * Launches actions for opening the skeleton viewer and neuroglancer.
@@ -15,23 +28,14 @@ function showSkeleton(id, dataset, actions) {
   actions.skeletonAddandOpen(id, dataset);
 }
 
-function BodyId (props) {
-  const { children, dataSet, actions } = props;
+function BodyId(props) {
+  const { children, dataSet, actions, classes } = props;
   return (
     <div>
-     <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row'
-        }}
-      >
-
+      <div className={classes.container}>
         <SelectAndCopyText text={children} actions={actions} />
         <Icon
-          style={{
-            marginLeft: '3px',
-            cursor: 'pointer'
-          }}
+          className={classes.icon}
           onClick={() => showSkeleton(children, dataSet, actions)}
           fontSize="inherit"
         >
@@ -42,11 +46,11 @@ function BodyId (props) {
   );
 }
 
-
 BodyId.propTypes = {
   children: PropTypes.number.isRequired,
   dataSet: PropTypes.string.isRequired,
-  actions: PropTypes.object.isRequired
+  actions: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
-export default BodyId;
+export default withStyles(styles)(BodyId);
