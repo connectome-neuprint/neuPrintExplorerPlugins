bb3af70ec5900324a6cd0d3d20a84d471d80a043
Jody Clements <jody@neomorphic.co.uk>
2019-11-14 14:15:59 -0500

    Adds SuperROI filtering to sunburst data loader.
    In order to show the same results as those displayed in the roi heat map
    breakdown, the ROIs need to be filtered so that they only show the
    superROIs.

--- packages/query/src/shared/SunburstLoader.jsx
+++ packages/query/src/shared/SunburstLoader.jsx
@@ -6,11 +6,16 @@ class SunburstLoader extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: {}
+      rawData: null,
+      data: {},
+      superROIs: null
     };
   }
 
   componentDidMount() {
+    // fetch information about super ROIs.
+    this.fetchSuperROIs();
+    // fetch information about inputs/outputs
     const { bodyId, dataSet } = this.props;
     const cypher = `MATCH (n :Neuron {bodyId: ${bodyId}})<-[:From]-(cs :ConnectionSet)-[:To]->(m) MATCH (n)-[x :ConnectsTo]->(m) RETURN m.bodyId, m.type, x.weight, cs.roiInfo, m.status, 'output' as direction UNION MATCH (n :Neuron {bodyId: ${bodyId}})<-[:To]-(cs :ConnectionSet)-[:From]->(m) MATCH (n)<-[x :ConnectsTo]-(m) RETURN m.bodyId, m.type, x.weight, cs.roiInfo, m.status, 'input' as direction`;
 
@@ -33,14 +38,45 @@ class SunburstLoader extends React.Component {
     fetch(queryUrl, querySettings)
       .then(result => result.json())
       .then(resp => {
-        this.constructDataObject(resp.data);
+        this.setState({ rawData: resp.data});
       })
       .catch(error => {
         console.log(error);
       });
   }
 
-  constructDataObject(rawData) {
+  fetchSuperROIs() {
+    const { dataSet } = this.props;
+    const cypher = `MATCH (n:Meta) RETURN n.superLevelRois`;
+
+    const parameters = {
+      cypher,
+      dataset: dataSet
+    };
+
+    const queryUrl = '/api/custom/custom';
+    const querySettings = {
+      headers: {
+        'content-type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(parameters),
+      credentials: 'include',
+      method: 'POST'
+    };
+
+    fetch(queryUrl, querySettings)
+      .then(result => result.json())
+      .then(resp => {
+        this.setState({ superROIs: resp.data[0][0]});
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+  }
+
+  constructDataObject(rawData, superROIs) {
     const { bodyId } = this.props;
     const data = {
       name: bodyId,
@@ -64,8 +100,8 @@ class SunburstLoader extends React.Component {
         const topLevel = data.children[dirPosition];
 
         const rois = JSON.parse(roisJSON);
-        // need to obtain the super ROI list from global state?
-        Object.entries(rois).forEach(([roiLabel, roiData]) => {
+        // TODO: need to obtain the super ROI list from global state?
+        Object.entries(rois).filter(entry => superROIs.includes(entry[0])).forEach(([roiLabel, roiData]) => {
           let roiLevel = topLevel.children.find(el => el.name === roiLabel);
           if (!roiLevel) {
             const roiObject = { name: roiLabel, children: [] };
@@ -85,14 +121,14 @@ class SunburstLoader extends React.Component {
 
       }
     });
-
-    this.setState({ data });
+    return data;
   }
 
   render() {
-    const { data } = this.state;
+    const { rawData, superROIs } = this.state;
     const colors = ['#396a9f', '#e2b72f'];
-    if (data.name) {
+    if (rawData && superROIs) {
+      const data = this.constructDataObject(rawData, superROIs);
       return <Sunburst data={data} colors={colors} />;
     }
     return <p>Loading</p>;
