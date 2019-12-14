31f572dad0f4247750cd98edbf82dfd18a9b8c0e
Jody Clements <jody@neomorphic.co.uk>
2019-12-14 16:39:51 -0500

    Syntax fix

--- packages/query/src/Neuroglancer.jsx
+++ packages/query/src/Neuroglancer.jsx
@@ -41,7 +41,7 @@ class Neuroglancer extends React.Component {
   static processResults(query) {
     return {
       debug: 'No cypher query for this plugin',
-      title: `Neuroglancer viewer for ${query.pm.dataSet}`,
+      title: `Neuroglancer viewer for ${query.pm.dataset}`,
     };
   };
 
