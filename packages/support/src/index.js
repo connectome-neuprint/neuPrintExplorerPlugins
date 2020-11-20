72a36f75510779b4abc239f130fd6aaaf3299151
Jody Clements <jody@neomorphic.co.uk>
2020-11-20 10:20:37 -0500

    Updates pickTextColorBasedOnBgColorAdvanced func
    This function was not coloring the text correctly to produce enough
    contrast. The changed function seems to do a better job.

--- packages/support/src/index.js
+++ packages/support/src/index.js
@@ -1,13 +1,13 @@
-export { default as TablePaginationActions } from "./TablePaginationActions";
-export { default as SunburstLoader } from "./SunburstLoader";
-export { default as SunburstFormatter } from "./SunburstFormatter";
-export { default as SkeletonLoader } from "./SkeletonLoader";
-export { default as SkeletonFormatter } from "./SkeletonFormatter";
-export { default as RoiInfoTip } from "./RoiInfoTip";
+export { default as TablePaginationActions } from './TablePaginationActions';
+export { default as SunburstLoader } from './SunburstLoader';
+export { default as SunburstFormatter } from './SunburstFormatter';
+export { default as SkeletonLoader } from './SkeletonLoader';
+export { default as SkeletonFormatter } from './SkeletonFormatter';
+export { default as RoiInfoTip } from './RoiInfoTip';
 
 export function metaInfoError(error) {
   return {
-    type: "META_INFO_ERROR",
+    type: 'META_INFO_ERROR',
     error
   };
 }
@@ -22,17 +22,11 @@ export function sortRois(a, b) {
 }
 
 export function pickTextColorBasedOnBgColorAdvanced(bgColor, lightColor, darkColor) {
-    const color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
-    const r = parseInt(color.substring(0, 2), 16); // hexToR
-    const g = parseInt(color.substring(2, 4), 16); // hexToG
-    const b = parseInt(color.substring(4, 6), 16); // hexToB
-    const uicolors = [r / 255, g / 255, b / 255];
-    const c = uicolors.map((col) => {
-          if (col <= 0.03928) {
-                  return col / 12.92;
-                }
-          return Math.pow((col + 0.055) / 1.055, 2.4);
-        });
-    const L = (0.2126 * c[0]) + (0.7152 * c[1]) + (0.0722 * c[2]);
-    return (L > 0.179) ? darkColor : lightColor;
+  const color = bgColor.charAt(0) === '#' ? bgColor.substring(1, 7) : bgColor;
+  const r = parseInt(color.substring(0, 2), 16); // hexToR
+  const g = parseInt(color.substring(2, 4), 16); // hexToG
+  const b = parseInt(color.substring(4, 6), 16); // hexToB
+  // Get YIQ ratio
+  const yiq = (r * 299 + g * 587 + b * 114) / 1000;
+  return (yiq >= 128) ? darkColor : lightColor;
 }
